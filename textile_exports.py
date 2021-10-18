fb3e93c26b602ce248b1de20d182f1412781b912
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2021-10-18 14:55:01 +0500

    Update textile_exports.py

--- textile_exports.py
+++ textile_exports.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components 
 import plotly.express as px 
 import pandas as pd 
+import xrld 
 
 
 st.set_page_config(page_title="Pakistan Textile Exports",
