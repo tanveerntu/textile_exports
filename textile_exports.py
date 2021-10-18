5600f5b0418bd9fb1ae7c274189a96b441c6a0f4
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2021-10-18 14:26:17 +0500

    Update textile_exports.py

--- textile_exports.py
+++ textile_exports.py
@@ -13,7 +13,7 @@ st.title('TEXTILE & CLOTHING EXPORTS FROM PAKISTAN')
 st.write("Source: Pakistan Bureau of Statistics")
 
 # to load dataset from computer as df
-df = pd.read_excel('monthly_textile_exports_pbs.xls')
+df=pd.read_excel('monthly_textile_exports_pbs.xls')
 df
 # Get sum of values in a column 'Exports_US$'
 # for those rows only where 'year' is '2020'
